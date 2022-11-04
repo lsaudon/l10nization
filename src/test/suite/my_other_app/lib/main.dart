9116015bd7a568233ec7e861278c65dca0482e71
lsaudon <lsaudon@gmail.com>
2022-11-04 13:16:54 +0100

    ♻️ Improve the conversion from text to key

--- src/test/suite/my_other_app/lib/main.dart
+++ src/test/suite/my_other_app/lib/main.dart
@@ -17,7 +17,7 @@ class App extends StatelessWidget {
       supportedLocales: AppLocalizations.supportedLocales,
       home: Scaffold(
         appBar: AppBar(title: Text(l10n.counterAppBarTitle)),
-        body: const Center(child: Text("Hello World")),
+        body: const Center(child: Text("Aujourd'hui il FAIT 50°C.")),
       ),
     );
   }

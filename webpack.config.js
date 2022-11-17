4db00fc4982603a5d99eef96f5b58a1af59908b1
lsaudon <lsaudon@gmail.com>
2022-11-17 19:35:34 +0100

    🔧 Cleanup webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,39 @@
-//@ts-check
-
-'use strict';
-
 const path = require('path');
 
-//@ts-check
-/** @typedef {import('webpack').Configuration} WebpackConfig **/
-
-/** @type WebpackConfig */
-const extensionConfig = {
-  target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-  mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-
-  entry: './src/extension/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
-  output: {
-    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2'
-  },
-  externals: {
-    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    // modules added here also need to be added in the .vscodeignore file
-  },
-  resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      }
-    ]
-  },
-  devtool: 'nosources-source-map',
-  infrastructureLogging: {
-    level: 'log' // enables logging required for problem matchers
-  }
+module.exports = () => {
+  /**
+   * @type {import("webpack").Configuration}
+   */
+  const config = {
+    entry: './src/extension/extension.ts',
+    externals: {
+      vscode: 'commonjs vscode'
+    },
+    infrastructureLogging: {
+      level: 'log'
+    },
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/u,
+          test: /\.ts$/u,
+          use: [
+            {
+              loader: 'ts-loader'
+            }
+          ]
+        }
+      ]
+    },
+    output: {
+      filename: 'extension.js',
+      libraryTarget: 'commonjs2',
+      path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    target: 'node'
+  };
+  return config;
 };
-module.exports = [extensionConfig];

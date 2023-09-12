9a4262085e22456d50863be6a9a0cc7cbed571c8
lsaudon <lsaudon@gmail.com>
2023-09-12 20:48:48 +0200

    💥 flutterPubGetEnabled to generationEnabled

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -4,7 +4,7 @@ import { EditFilesCommand } from '../commands/editFilesCommand';
 import { EditFilesParameters } from '../commands/editFilesParameters';
 import { InputBoxCommand } from '../commands/inputBoxCommand';
 import { LocalizationActionProvider } from '../codeActions/localizationActionProvider';
-import { applySaveAndRunFlutterPubGet } from './applySaveAndRunFlutterPubGet';
+import { applySaveAndRunGeneration } from './applySaveAndRunFlutterPubGet';
 import { setEditFilesParameters } from './setEditFilesParameters';
 import { sortAndSave } from './sortAndSave';
 
@@ -21,10 +21,7 @@ export function activate(context: vscode.ExtensionContext): void {
     }),
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand(
-      EditFilesCommand.commandName,
-      async (...args: EditFilesParameters[]): Promise<void> => applySaveAndRunFlutterPubGet(args[0]),
-    ),
+    vscode.commands.registerCommand(EditFilesCommand.commandName, async (...args: EditFilesParameters[]): Promise<void> => applySaveAndRunGeneration(args[0])),
   );
   context.subscriptions.push(vscode.commands.registerCommand('l10nization.sortArbFiles', async (): Promise<void> => sortAndSave()));
 }

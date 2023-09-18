4c844a7a2515e91b35cb9c81d968ba84bd667279
lsaudon <lsaudon@gmail.com>
2023-09-18 21:09:01 +0200

    fix: haveDescription #314

--- src/shared/configuration.ts
+++ src/shared/configuration.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 const getConfiguration = vscode.workspace.getConfiguration('l10nization');
 export const getYamlFileName = getConfiguration.get<string>('yamlFile', 'l10n.yaml');
 export const getAppLocalizationsVariable = getConfiguration.get<string>('appLocalizationsVariable', 'l10n');
-export const getHaveDescription = getConfiguration.get<boolean>('haveMetadatas', false);
+export const getHaveDescription = getConfiguration.get<boolean>('haveDescription', false);
 export const getCopyMetadataInAllFiles = getConfiguration.get<boolean>('copyMetadataInAllFiles', true);
 export const getSortArbEnabled = getConfiguration.get<boolean>('arbSort', true);
 export const getGenerationActivted = getConfiguration.get<boolean>('flutterPubGetEnabled', true);

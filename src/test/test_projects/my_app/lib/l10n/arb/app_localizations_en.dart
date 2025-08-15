cf676472aa180dcba5864f06955a65613f677be1
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2025-08-15 11:56:05 +0200

    chore(dart): upgrade deps

---
+++ src/test/test_projects/my_app/lib/l10n/arb/app_localizations_en.dart
@@ -0,0 +1,10 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+}
